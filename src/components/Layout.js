267f7a67c13c95c3521d5b08b26b6d46f6c7e94d
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-06 15:17:01 -0400

    Renamed files

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 // import { useStaticQuery, graphql } from "gatsby"
 
-import GlobalStyle from "../styles/globalStyle"
+import GlobalStyle from "../styles/GlobalStyle"
 
 const Layout = ({ children }) => {
   // const data = useStaticQuery(graphql`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
+import Bio from "../components/Bio";
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hamza</h1>
+    <Bio></Bio>
   </Layout>
 )
 
