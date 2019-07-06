46d936f735191b0a028adeaf37feeaa6b767ac00
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-06 13:09:01 -0400

    Created styles and basic theme

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+const theme = {
+    colors: {
+      lightNavy: '#34374C',
+      darkNavy: '#2C2E3E',
+      red: '#EE2B47',
+      lightGrey: '#F6F6F6',
+    },
+
+    fonts: {
+      Rubik: 'Rubik',
+      RobotoMono: 'Roboto Mono'    
+    }
+  };
+  
+  export default theme;
