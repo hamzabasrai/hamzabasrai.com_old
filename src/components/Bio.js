ff08e71c604cc21b0b47d07952309f8ee6fa3d65
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-06 15:19:41 -0400

    Created Bio component

---
+++ src/components/Bio.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import styled from "styled-components"
+import Heading from '../styles/Heading';
+
+
+export class Bio extends Component {
+    
+    render() {
+        return (
+            <div>
+                <Heading>Hi, I'm Hamza</Heading>
+            </div>
+        )
+    }
+}
+
+export default Bio
