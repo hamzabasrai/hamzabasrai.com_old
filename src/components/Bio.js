f26b7a3d82bc31c9a3925d69c4f4631f0541256f
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-09 21:31:41 -0400

    Fixed security issue with rel property

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -13,8 +13,8 @@ export class Bio extends Component {
           I am an enthusiastic software engineering student that loves to solve
           interesting problems. I'm a big fan of product design and creating
           software that impacts the lives of others. Outside of school, I lead
-          the organizing team of an MLH Hackathon called <a href="https://2019.uottahack.ca" target="_blank">uOttaHack</a> at the
-          University of Ottawa. I'm an avid reader and like to discover new
+          the organizing team of an MLH Hackathon called <a href="https://2019.uottahack.ca" target="_blank" rel="noopener noreferrer">uOttaHack</a> at the
+          University of Ottawa. I'm an avid <a>reader</a> and like to discover new
           music.
         </Body>
       </Container>
