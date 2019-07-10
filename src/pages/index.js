194db3869df1da652bdfede88f00ed2d844c5440
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-09 22:23:22 -0400

    Added grid to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React from "react";
+import styled from "styled-components";
 
 import Layout from "../components/Layout";
 import Bio from "../components/Bio";
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  padding: 15px;
+
+  div {
+    grid-column-start: 2;
+    grid-column-end: 3;
+  }
+`;
+
 const IndexPage = () => (
   <Layout>
-    <Bio></Bio>
+    <Grid>
+      <Bio></Bio>
+    </Grid>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
