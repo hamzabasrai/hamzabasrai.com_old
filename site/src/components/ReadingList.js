e0b7374526d172d9f2a54b528fcc111d985c88ec
Hamza Basrai <hamza.basrai@gmail.com>
2019-07-13 12:59:00 -0400

    Added API call to ReadingList

--- site/src/components/ReadingList.js
+++ site/src/components/ReadingList.js
@@ -2,17 +2,34 @@ import React, { Component } from "react";
 import Container from "../styles/Container";
 import Heading from "../styles/Heading";
 import Body from "../styles/Body";
+import styled from "styled-components";
 import axios from "axios";
 
+const CurrentBook = styled.img`
+  max-width: 150px;
+`
+
 export class ReadingList extends Component {
+  state = {
+    book: {
+      title: "",
+      image: ""
+      
+    }
+  };
 
   componentDidMount() {
-    this.getBook();
+   this.getBook();
   }
 
-  
   getBook() {
-    axios.get();
+    // .get("https://us-central1-hamzabasrai-site.cloudfunctions.net/helloWorld")
+    axios
+      .get("http://localhost:5000/hamzabasrai-site/us-central1/helloWorld")
+      .then(val => {
+        console.log(val);
+        this.setState({book: val.data});
+      });
   }
 
   render() {
@@ -21,8 +38,9 @@ export class ReadingList extends Component {
         <Heading>Reading List</Heading>
         <Body>
           Here’s a look at what I’m reading and what I plan to read next. I’m
-          always looking for recommendations so feel free to drop one below
+          always looking for recommendations so feel free to drop one below 
         </Body>
+        <CurrentBook src={this.state.book.image} alt={this.state.book.title}/>
       </Container>
     );
   }
