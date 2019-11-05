cd19a788175941d479402abb2c31fd4e07e4a0f9
Hamza Basrai <hamza.basrai@gmail.com>
2019-11-05 14:36:07 -0500

    Updated Section padding and images

--- site/src/styles/Section.js
+++ site/src/styles/Section.js
@@ -4,18 +4,20 @@ import media from "../styles/media";
 const Section = styled.section`
   display: flex;
   flex-direction: column;
-  padding: 65% 10% 15% 10%;
+  padding-top: 45%; 
+  padding-left: 10%; 
+  padding-right: 10%;
 
   @media screen and (min-width: 768px) {
-    padding: 35% 10% 15% 10%;
+    padding-top: 35%;
   }
 
   @media screen and (min-width: 1024px) {
-    padding: 25% 10% 15% 10%;
+    padding-top: 25%;
   }
 
   @media screen and (min-width: 1440px) {
-    padding: 12% 10% 6% 10%;
+    padding-top: 15%;
   }
 `;
 
