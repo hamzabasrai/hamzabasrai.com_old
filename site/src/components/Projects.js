7b2e32d71b8d93fd071d443433d6c156637c00e4
Hamza Basrai <hamza.basrai@gmail.com>
2019-11-04 23:48:22 -0500

    Added content to projects component

--- site/src/components/Projects.js
+++ site/src/components/Projects.js
@@ -1,13 +1,86 @@
 import React, { Component } from "react";
 import Section from "../styles/Section";
 import Heading from "../styles/Heading";
+import styled from "styled-components";
+import Card from "../styles/Card";
 
+const ProjectGrid = styled.div`
+  display: grid;
+  padding: 10% 0;
+  grid-template-rows: repeat(3, 1fr);
+  grid-row-gap: 25px;
+`;
+
+const Project = styled(Card)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 10px;
+
+  .heading {
+    grid-area: 1 / 1 / 2 / 3;
+    justify-self: self-start;
+    align-self: center;
+  }
+
+  .description {
+    grid-area: 2 / 1 / 4 / 4;
+    padding: 0;
+  }
+
+  .date {
+    justify-self: self-end;
+    align-self: center;
+    grid-area: 1 / 3 / 2 / 4;
+  }
+
+  .action {
+    justify-self: center;
+    align-self: center;
+    grid-area: 4 / 1 / 5 / 4;
+  }
+`;
 
 export class Projects extends Component {
   render() {
     return (
       <Section>
         <Heading>Things I've Built</Heading>
+        <ProjectGrid>
+          <Project className="">
+            <h1 className="heading">hamzabasrai.com</h1>
+            <p className="description">
+              My personal website/workspace where you're reading this right now
+            </p>
+            <p className="date">2019</p>
+            <button type="button" className="action">
+              View Code
+            </button>
+          </Project>
+          <Project>
+            <h1 className="heading">TalentBoard</h1>
+            <p className="description">
+              A web application for companies to track new job applicants
+              throughout their interview process.
+            </p>
+            <p className="date">2018</p>
+            <button type="button" className="action">
+              View Demo
+            </button>
+          </Project>
+          <Project>
+            <h1 className="heading">Duty Helper</h1>
+            <p className="description">
+              An Android application to gameify household chores for famillies.
+              Parents can assign chores to their kids who in turn, earn points.
+            </p>
+            <p className="date">2017</p>
+            <button type="button" className="action">
+              View Code
+            </button>
+          </Project>
+        </ProjectGrid>
       </Section>
     );
   }
--- site/src/styles/Card.js
+++ site/src/styles/Card.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 const Card = styled.div`
-  background-color: #e3e3e3;
+  background-color: #ffffff;
   border-radius: 15px;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  padding: 20px;
+  padding: 15px;
 
   &:hover {
     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
